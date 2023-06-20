ed644b398e297a244353a95f30fd535dcc0af254
angryWhy <875978822@qq.com>
2023-06-20 21:50:54 +0800

    menu service

--- src/service/product/product.js
+++ src/service/product/product.js
@@ -1,7 +1,25 @@
 import request from '../request';
 //springboot接口
-export function menuService(){
+export function menuList(){
     return request({
         url:"/product/pmscategory/list/tree"
+    })
+}
+export function menuAdd(params){
+    return request({
+        url:"/product/pmscategory/list/tree",
+        data:{
+            ...params   
+        },
+        method:"POST"
+    })
+}
+export function menuDel(params){
+    return request({
+        url:"/product/pmscategory/list/tree",
+        data:{
+            ...params   
+        },
+        method:"POST"
     })
 }
--- src/views/product/category.vue
+++ src/views/product/category.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        <el-tree :data="dataMenu" :props="defaultProps" @node-click="handleNodeClick" />
+        <el-tree :data="dataMenu" :props="defaultProps" :expand-on-click-node="false" :node-key="catId">
+            <template #default="{ node, data }">
+                <span class="custom-tree-node">
+                    <span>{{ node.label }}</span>
+                    <span>
+                        <el-button v-if="node.level<=2" @click="treeAdd(data)" link type="primary"> 添加 </el-button>
+                        <el-button v-if="node.childNodes.length==0" @click="treeDelete(node, data)" link type="danger"> 删除 </el-button>
+                    </span>
+                </span>
+            </template>
+        </el-tree>
     </div>
 </template>
 
 <script>
-import { defineComponent,ref,onMounted} from 'vue'
-import {menuService} from "@/service/product/product"
+import { defineComponent, ref, onMounted } from 'vue'
+import { menuList,menuAdd,menuDel } from "@/service/product/product"
 
 export default defineComponent({
     name: 'categoryView',
@@ -15,19 +25,27 @@ export default defineComponent({
         const handleNodeClick = (data) => {
             console.log(data)
         }
-        onMounted(()=>{
-            menuService().then(res=>{
-                dataMenu.value = res.data;
-        })
-        })
+        const treeAdd = (data) => {
+
+        }
+        const treeDelete = (node, data) => {
+            
+        }
         const defaultProps = {
             children: 'children',
             label: 'name',
         }
+        onMounted(() => {
+            menuList().then(res => {
+                dataMenu.value = res.data;
+            })
+        })
         return {
             dataMenu,
             handleNodeClick,
-            defaultProps
+            defaultProps,
+            treeAdd,
+            treeDelete
         }
     }
 })
